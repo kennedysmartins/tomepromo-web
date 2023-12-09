e5d1541f8fff2d08bc58862d5dc1496e189c3e40
KillerDogs <kenybustamante2@hotmail.com>
2023-12-08 21:52:24 -0300

    remove middleware

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,13 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import GoogleProvier from "next-auth/providers/google"
 
 export const authOptions = {
   providers: [
-    GoogleProvider({
+    GoogleProvier({
       clientId: process.env.GOOGLE_ID ?? "",
       clientSecret: process.env.GOOGLE_SECRET ?? "",
     }),
   ],
-  callbacks: {
-    async signInCallback(url, account, profile) {
-      return Promise.resolve(true);
-    },
-  },
 }
 
 const handler = NextAuth(authOptions)
--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,27 @@
-import { NextRequest, NextResponse } from "next/server"
-import { getToken } from "next-auth/jwt"
+// import { NextRequest, NextResponse } from "next/server"
+// import { getToken } from "next-auth/jwt"
 
-export default function middleware(request: NextRequest) {
-  console.log("Middleware")
-  const token = request.cookies.get("next-auth.session-token")?.value
+// export default function middleware(request: NextRequest) {
+//   console.log("Middleware")
+//   const token = request.cookies.get("next-auth.session-token")?.value
 
-  const signInURL = new URL("/auth", request.url)
-  const dashboardURL = new URL("/dashboard", request.url)
+//   const signInURL = new URL("/auth", request.url)
+//   const dashboardURL = new URL("/dashboard", request.url)
 
-  if (!token) {
+//   if (!token) {
 
-    if (request.nextUrl.pathname == "/auth") {
+//     if (request.nextUrl.pathname == "/auth") {
 
-      return NextResponse.next()
-    }
-    return NextResponse.redirect(signInURL)
-  }
+//       return NextResponse.next()
+//     }
+//     return NextResponse.redirect(signInURL)
+//   }
 
-  if(request.nextUrl.pathname == "/auth"){
-    return NextResponse.redirect(dashboardURL)
-  }
-}
+//   if(request.nextUrl.pathname == "/auth"){
+//     return NextResponse.redirect(dashboardURL)
+//   }
+// }
 
-export const config = {
-  matcher: ["/auth", "/dashboard/:path*", "/products/:path*", "/users/:path*"],
-}
+// export const config = {
+//   matcher: ["/auth", "/dashboard/:path*", "/products/:path*", "/users/:path*"],
+// }
