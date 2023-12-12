5341df17c017ca3bf9c00b9ee928355cf5f58a59
KillerDogs <kenybustamante2@hotmail.com>
2023-12-12 11:07:11 -0300

    middleware login

---
+++ middleware.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server"
+import { getToken } from "next-auth/jwt"
+
+export default function middleware(request: NextRequest) {
+  console.log("Middleware")
+  const token = request.cookies.get("__Secure-next-auth.session-token")?.value
+
+  const signInURL = new URL("/auth", request.url)
+  const dashboardURL = new URL("/dashboard", request.url)
+
+  if (!token) {
+
+    if (request.nextUrl.pathname == "/auth") {
+
+      return NextResponse.next()
+    }
+    return NextResponse.redirect(signInURL)
+  }
+
+  if(request.nextUrl.pathname == "/auth"){
+    return NextResponse.redirect(dashboardURL)
+  }
+}
+
+export const config = {
+  matcher: ["/auth", "/dashboard/:path*", "/products/:path*", "/users/:path*"],
+}
